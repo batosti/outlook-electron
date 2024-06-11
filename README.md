2e4ef3bf46ce004b9292a345e51d7de3d2036b2e
Andre Batosti <andre.batosti@raizen.com>
2024-06-11 09:00:31 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Outlook Electron
+
+Outlook client made with electron and Outlook Web App
+
+## Setup
+
+`npm install`
+
+# Run 
+
+`npm run start`
+
+# Package
+
+They will generate `.deb` and `snap` packages
+
+`npm run dist`
+
+## Packages
+
+![Get it from the Snap Store](https://snapcraft.io/static/images/badges/en/snap-store-black.svg)(https://snapcraft.io/outlook-electron)
+
+## Support this project
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/yellow_img.png)](https://www.buymeacoffee.com/batosti)
---
+++ app/index.js
@@ -0,0 +1,78 @@
+const { app, BrowserWindow, shell } = require('electron');
+var path = require('path')
+
+const appUrl = 'https://outlook.office.com/'
+
+/**
+ * @type {BrowserWindow}
+ */
+let window = null;
+
+/**
+ * @param {Electron.HandlerDetails} details 
+ * @returns {action: 'deny'}
+ */
+function onNewWindow(details) {
+  if (details.url !== 'about:blank') {
+	shell.openExternal(details.url);
+	  return { action: 'deny' };
+  }
+  return { action: 'allow' }
+}
+
+const createWindow = () => {
+  window = new BrowserWindow({
+    icon: path.join(__dirname, 'assets/icons/icon-96x96.png'),
+	autoHideMenuBar: true
+  });
+  window.maximize();
+  window.loadURL(appUrl)
+
+	window.webContents.setWindowOpenHandler(onNewWindow);
+}
+
+const appLock = app.requestSingleInstanceLock();
+
+const protocolClient = 'mailto';
+if (!app.isDefaultProtocolClient(protocolClient, process.execPath)) {
+	app.setAsDefaultProtocolClient(protocolClient, process.execPath);
+}
+
+if (!appLock) {
+	app.quit();
+} else {
+  app.on('second-instance', onAppSecondInstance);
+  app.on('ready', onAppReady);
+}
+async function onAppReady() {
+	createWindow();
+};
+
+function processArgs(args) {
+	var regHttps = /^https:\/\/outlook\.office\.com\/.*/g;
+	var regMailTo = /^mailto:\/.*/g;
+	for (const arg of args) {
+		if (regHttps.test(arg)) {
+			return arg;
+		}
+		if (regMailTo.test(arg)) {
+			return appUrl + '/owa/?path=/mail/action/compose&to=' + arg.substr(7).replace('?', '&');
+		}
+	}
+}
+
+function onAppSecondInstance(event, args) {
+	console.debug('second-instance started');
+	if (window) {
+		event.preventDefault();
+		const url = processArgs(args);
+		if (url) {
+			allowFurtherRequests = false;
+			setTimeout(() => { allowFurtherRequests = true; }, 5000);
+			window.loadURL(url, { userAgent: config.chromeUserAgent });
+		}
+
+    window.focus();
+  }
+};
+
