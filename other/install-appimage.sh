365120ef83b6d65ec713a959dd3f88e8e7adae6e
Guy Keller <guy.keller@firstmac.com.au>
2025-08-13 16:18:17 +1000

    fixing download urls, it was getting the html instead of the file contents

--- other/install-appimage.sh
+++ other/install-appimage.sh
@@ -10,14 +10,14 @@ wget https://github.com/guy-keller/outlook-electron/releases/download/1.1.0/outl
 chmod +x outlook-electron-1.1.0.AppImage
 
 # Downloads icon file and bash script that runs the app
-wget https://github.com/guy-keller/outlook-electron/tree/main/other/outlook-electron.png
-wget https://github.com/guy-keller/outlook-electron/tree/main/other/outlook-electron.sh
+wget https://raw.githubusercontent.com/guy-keller/outlook-electron/refs/heads/main/other/outlook-electron.png
+wget https://raw.githubusercontent.com/guy-keller/outlook-electron/refs/heads/main/other/outlook-electron.sh
 chmod +x outlook-electron.sh
 
 # Downloads the desktop file, so the user has a nice shortcut
 cd ~/.local/share/applications/
-wget https://github.com/guy-keller/outlook-electron/tree/main/other/outlook-electron.desktop
+wget https://raw.githubusercontent.com/guy-keller/outlook-electron/refs/heads/main/other/outlook-electron.desktop
 
 # Refresh the menu, so that the app icon is displayed
-sudo update-desktop-database /usr/share/applications
+sudo update-desktop-database ~/.local/share/applications/
 sudo xdg-desktop-menu forceupdate
